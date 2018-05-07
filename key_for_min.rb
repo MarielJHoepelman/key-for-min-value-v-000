ff291d793c02ec0ec134be444176c08d8c054e51
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-07 01:12:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smaller_key = nil
   smaller_value = nil
   name_hash.collect do |key, value|
-    if smaller_value = nil || value < smaller_value 
+    if smaller_value == nil || value < smaller_value 
       smaller_value = value
       smaller_key = key
     end
